7785207d81967104acb75da32f420854bef450fa
Arshpreet Singh <arshsomal100@gmail.com>
2025-03-09 19:35:54 +0530

    Added new product routes

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -30,11 +30,43 @@ export const createProduct = async (req: Request, res: Response, next: NextFunct
 export const getProducts = async (req: Request, res: Response, next: NextFunction) : Promise<void> => {
     try {
         const  products = await prisma.product.findMany({
-            take : 6
+            take : 6,
+            orderBy : {
+                createdAt : 'desc'
+            }
         });
 
         res.status(200).json({
             products
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateProduct = async (req: Request, res: Response, next: NextFunction) : Promise<void> => {
+    try {
+        const { productId } = req.params;
+        const parsedData = productSchema.safeParse(req.body);
+
+        if(parsedData.error){
+            res.status(400).json({
+                message : "All fields are reuired",
+                error : parsedData.error
+            })
+            return;
+        }
+
+        await prisma.product.update({
+            where : {
+                id : productId
+            },
+            data : parsedData.data
+        });
+
+        res.status(200).json({
+            messgae : "Product updated successfully",
+            success : true,
         })
     } catch (error) {
         next(error);
--- backend/src/routes/product.routes.ts
+++ backend/src/routes/product.routes.ts
@@ -1,10 +1,12 @@
 import express, { Router } from "express";
 import { isAuthenticated } from "../middlewares/isAuthenticated";
 import { isAdmin } from "../middlewares/isAdmin";
-import { createProduct, getProducts } from "../controllers/product.controller";
+import { createProduct, getProducts, updateProduct } from "../controllers/product.controller";
 const router : Router = express.Router();
 
 router.route('/').post(isAuthenticated, isAdmin, createProduct);
+
+router.route('/').put(isAuthenticated, isAdmin, updateProduct);
 
 router.route('/').get(getProducts);
 
